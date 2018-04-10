4f815d785d9eeb13d53ddc0f1479ca85d41bd418
murloc87 <a.a.homenko@yandex.ru>
2018-04-10 11:04:20 +0300

    Костыль модального окна в owl

--- default/resources/views/partials/gallery-owl.blade.php
+++ default/resources/views/partials/gallery-owl.blade.php
@@ -1,10 +1,18 @@
 {{-- Управление изображениями галереи --}}
 
 @push('footer-scripts')
-    <script src="{{ mix('frontend/plugins/bootstrap-v3/modal.js') }}" defer></script>
-    <script src="{{ mix('frontend/partials/gallery.js') }}" defer></script>
     <link rel="stylesheet" href="{{ mix('frontend/styles/gallery.css') }}" />
     <link rel="stylesheet" href="{{ mix('frontend/styles/gallery-owl.css') }}" />
+    <script src="{{ mix('frontend/plugins/bootstrap-v3/modal.js') }}" defer></script>
+    <script src="{{ mix('frontend/partials/gallery.js') }}" defer></script>
+    <script>
+        $("#modalImage").on("shown.bs.modal", function () {
+            $(this).css("display", "block");
+        });
+        $("button.close").click(function () {
+            $("#modalImage").removeAttr("style").removeClass("in");
+        });
+    </script>
 @endpush
 
 <div id="gallery-owl">
