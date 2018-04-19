91d97454fa562c48d2581686ccab92e78afd6e6e
BBM <admin@velib.com>
2018-04-19 16:40:35 +0300

    исправлен баг URL, если нет изображений

--- src/Services/Blade/SrcFiles.php
+++ src/Services/Blade/SrcFiles.php
@@ -18,27 +18,27 @@ class SrcFiles
 
     public function thumb($items)
     {
-        return $this->getFirstFile($items)->src_thumb;
+        return count($items->filesActual) > 0 ?  $this->getFirstFile($items)->src_thumb : null ;
     }
 
     public function preview($items)
     {
-        return $this->getFirstFile($items)->src_preview;
+        return count($items->filesActual) > 0 ?  $this->getFirstFile($items)->src_preview : null;
     }
 
     public function full($items)
     {
-        return $this->getFirstFile($items)->src_full;
+        return count($items->filesActual) > 0 ? $this->getFirstFile($items)->src_full : null;
     }
 
     public function original($items)
     {
-        return $this->getFirstFile($items)->src_original;
+        return count($items->filesActual) > 0 ? $this->getFirstFile($items)->src_original : null;
     }
 
     private function getFirstFile($items)
     {
-        return array_first($items->filesActual->sortBy('order'));
+        return count($items->filesActual) > 0 ? array_first($items->filesActual->sortBy('order')) : null;
     }
 
 }
